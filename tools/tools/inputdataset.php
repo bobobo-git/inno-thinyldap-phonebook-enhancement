fbbeacbb34922e782c397da8b900ea7ffe5e5517
bobobo-git <bobobo@gmx.de>
2018-11-13 12:39:44 +0100

    up

---
+++ tools/tools/inputdataset.php
@@ -0,0 +1,29 @@
+<?php
+/** Input one Number to address without Company, name or other stuff.
+*/
+require("dbconnect.php");
+if ($db->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+}
+
+/*
+$_GET["ptype"]=home&$_GET["number"]=+491622123321&$_GET["company"]=hubert%20müller&$_GET["lastname"]=asdasd&$_GET["firstname"]=eerrr
+*/
+$company=$_GET["company"];
+$lastname=$_GET["lastname"];
+$firstname=$_GET["firstname"];
+
+$ins='insert into address ('.$_GET["ptype"].',company,lastname,firstname) values (\''.urlencode($_GET["number"]).'\',\''.urlencode($_GET["company"]).'\',\''.urlencode($_GET["lastname"]).'\',\''.urlencode($_GET["firstname"]).'\');';
+//values ('+491622123321'.'emil'.'hugo'.'franz');
+//echo $ins;
+if (    !$db->query($ins))
+	{
+		echo "nok";
+		echo " ". $ins;
+	}
+else{
+	echo "ok";
+	}
+require("dbdisconnect.php");
+
+?>
